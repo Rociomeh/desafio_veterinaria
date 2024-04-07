dfa17e69ddd2324cf12c05333b11690133b27de2
Rocío <rociommaturana@gmail.com>
2024-04-06 23:50:11 -0300

    fixed: desafio terminado

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const fs = require('fs')
- const operaciones = [`
- Registrar:
-    Nombre del animal
-    Edad
-    Tipo de animal
-    Color del animal
-    Enfermedad
-`]
-fs.writeFileSync('operaciones.js', operaciones)
+const fs = require('fs');
+const operaciones = require('./operaciones');
+const operacion = process.argv[2];
+const args = process.argv.slice(3);
 
-const { vaciarJSON } = require('./operaciones') 
-vaciarJSON("citas.json")
+if (operacion === 'registrar') {
+
+  const [nombre, edad, tipo, color, enfermedad] = args;
+  operaciones.registrar(nombre, edad, tipo, color, enfermedad);
+
+} else if (operacion === 'leer') {
+  operaciones.leer();
+
+} else {
+  console.error('Operación no válida. Las opciones son: registrar o leer.');
+}
--- operaciones.js
+++ operaciones.js
@@ -1,7 +1,55 @@
+const fs = require('fs');
+const archivoCitas = './citas.json';
 
- Registrar:
-    Nombre del animal
-    Edad
-    Tipo de animal
-    Color del animal
-    Enfermedad
+function registrar(nombre, edad, tipo, color, enfermedad) {
+
+  const cita = {
+    nombre,
+    edad,
+    tipo,
+    color,
+    enfermedad,
+  };
+
+  leerCitas((citas) => {
+    citas.push(cita);
+    escribirCitas(citas);
+  });
+}
+
+function leer() {
+  leerCitas((citas) => {
+
+    console.log(citas);
+  });
+}
+
+function leerCitas(callback) {
+
+  fs.readFile(archivoCitas, 'utf-8', (err, data) => {
+    if (err) {
+      console.error('Error al leer el archivo de citas:', err);
+      return;
+    }
+    const citas = JSON.parse(data) || [];
+    callback(citas);
+  });
+}
+
+function escribirCitas(citas) {
+
+  const jsonData = JSON.stringify(citas);
+
+  fs.writeFile(archivoCitas, jsonData, (err) => {
+    if (err) {
+      console.error('Error al escribir el archivo de citas:', err);
+      return;
+    }
+    console.log('Cita registrada correctamente.');
+  });
+}
+
+module.exports = {
+  registrar,
+  leer,
+};
